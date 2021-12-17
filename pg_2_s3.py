6e0f27df87e14a0edf4bc3fc9478dbc5545cee65
Vaibhav Gupta <vaibhav.gupta@hexaviewtech.com>
2021-12-17 13:55:49 +0530

    d

--- pg_2_s3.py
+++ pg_2_s3.py
@@ -2,10 +2,12 @@ import os
 import boto3
 
 
-session = boto3.Session(
-    aws_access_key_id=os.environ['%env.AWS_AK%'],
-    aws_secret_access_key=os.environ['%env.AWS_SK%'],
-)
+#session = boto3.Session(
+#    aws_access_key_id=os.environ['%env.AWS_AK%'],
+#    aws_secret_access_key=os.environ['%env.AWS_SK%'],
+#)
+
+session = boto3.session.Session(profile_name='ankit9')
 s3 = session.resource('s3')
 
 s3.meta.client.upload_file(Filename='pg_backup.sql', Bucket='cloudfrontpractic', Key='pg_backup.sql')
